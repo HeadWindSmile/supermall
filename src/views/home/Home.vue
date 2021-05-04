e3a0694a2e599bc417490571c93f1e056e3c622b
HeadWindSmile <947857914@qq.com>
2021-05-04 11:30:02 +0800

    1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners = 'banners'></home-swiper>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from 'network/home'
+  import NavBar from 'components/common/navbar/NavBar';
+  import HomeSwiper from './childComps/HomeSwiper'
+
+
+  import {getHomeMultidata} from 'network/home';
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
     },
     created() {
       getHomeMultidata().then(res => {
-        console.log(res)
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
       })
     }
   }
